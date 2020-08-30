94337bea024810dd699a35654c2acc0d2d0355c1
Tuatarian <67530629+Tuatarian@users.noreply.github.com>
2020-08-30 00:02:52 -0700

    Update AdventOfCode2015Day4.nim

--- AdventOfCode2015Day4.nim
+++ AdventOfCode2015Day4.nim
@@ -1,19 +1,17 @@
 import md5
-var key = "wtnhxymk"
+var key = "wtnhxymk" # Replace this with the key given to you by the site
 
-proc `++`(i : var int) = i += 1
+proc `++`(i : var int) = i += 1  # Simple increment helper proc
 
 proc gen(s : string) : seq[char] =
-    var i = 0
+    var i = 0  # The ticking int required by the challenge
     while true:
-        var hash = getMD5(s & $i)
-        if hash[0..4] == "00000":
-            result.add(hash[5])
-            echo result
-            echo s & $i
+        var hash = getMD5(s & $i) # Requires importing md5. Counts as a side effect, so I have to use proc keyword
+        if hash[0..4] == "00000":  # x..y defines the range to check substr in
+            result.add(hash[5]) 
         if result.len == 8:
-            return result
-        ++i
+            return result  # return keyword breaks the while and exits the proc
+        ++i 
 
-for c in key.gen:
+for c in key.gen:  # This just writes out the digits on the same line as a string for easy submission
     stdout.write(c)
