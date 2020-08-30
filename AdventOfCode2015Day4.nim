800e0ca375cbe5fd1abe15d409cd4ad946d91715
Tuatarian <67530629+Tuatarian@users.noreply.github.com>
2020-08-30 01:33:23 -0700

    Update AdventOfCode2015Day4.nim

--- AdventOfCode2015Day4.nim
+++ AdventOfCode2015Day4.nim
@@ -7,7 +7,7 @@ proc `++`(i : var int) = i += 1
 proc gen(s : string) : int =
     var i : int
     while true:
-        if getMD5(s & $i)[0..5] == "000000":
+        if getMD5(s & $i)[0..4] == "00000":
             return i
         ++i
 
